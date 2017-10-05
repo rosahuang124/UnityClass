b137c731c00e4805a6a864b5dbc0ec4521f51797
rosahuang124 <rosahuang124@gmail.com>
2017-10-05 17:00:50 -0400

    week4 hw

---
+++ week4_homework/Assets/Scripts/force.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class force : MonoBehaviour {
+
+	public Vector3 forceVector;
+	public vectorDrawer forceSource;
+	public Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		forceVector = Vector3.forward;
+		forceVector = forceSource.vector;
+		rb.GetComponent<Rigidbody> ();
+		rb.AddForce (forceVector); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		
+	}
+}
---
+++ week4_homework/Assets/Scripts/vectorDrawer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class vectorDrawer : MonoBehaviour {
+
+	public Vector3 vector;
+	[Range(1000, 4000)]
+	public float scale = 4000;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnDrawGizmos(){
+		vector = transform.forward*scale;
+		Gizmos.color = Color.yellow;
+		Gizmos.DrawLine (transform.position, transform.position + vector);
+	}
+}
