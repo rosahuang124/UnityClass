93fecb9a6141e989860ef9e7401e0d92c7897820
rosahuang124 <rosahuang124@gmail.com>
2017-10-19 18:14:56 -0400

    w6

---
+++ week6_homework/Assets/scripts/lightColor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lightColor : MonoBehaviour {
+	public Color colorA;
+	public Color colorB;
+
+	private Light spotLight;
+
+	public float increment;
+
+	[Range(0,1)]
+	public float t;
+
+	// Use this for initialization
+	void Start () {
+		spotLight = GetComponent<Light> ();
+		t = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		t += increment;
+
+		if (t <= 0 || t >= 1) {
+			increment *= -1f;
+		}
+		spotLight.color = Color.Lerp (colorA, colorB, t);
+	}
+}
---
+++ week6_homework/Assets/scripts/lightController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lightController : MonoBehaviour {
+
+	//private Light spotLight;
+
+
+	void Start () {
+		transform.Rotate (0, Time.time,0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		int t1 = Random.Range(30, 60);
+		int t2 = Random.Range(30, 60);
+
+		transform.Rotate (Time.deltaTime * t1, Time.deltaTime * t2, Time.deltaTime * t2 ); 
+	}
+}
